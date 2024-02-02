f699f95876b6448168acc537c13c7651c62af9a1
BigShuff <stevedavis0504@gmail.com>
2024-02-02 15:17:33 +0000

    finished

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,108 +1,98 @@
-// const myElement = document.createElement('div');
-// myElement.style.width = '100px';
-// myElement.style.height = '100px';
-// myElement.style.background = 'red';
-// myElement.style.transition = 'background-color 1s ease';
+const educationTxtContent = 'Some people begin to make a plan for paying education tuition as soon as their child is born. Even if you didn’t start early, education planning can help you put your child through education without facing unexpectedly large expenditures. A financial advisor can guide you in understanding the best ways to save for future education costs and how to fund potential gaps, explain how to reduce out-of-pocket costs, and advise you on eligibility for financial aid and grants.';
 
-// // add the element to your page's HTML
-// document.body.appendChild(myElement);
+const personalTxtContent = 'With a personal pension you pay regular monthly amounts or a lump sum to a pension provider who invests it on your behalf. Other people and family members can pay a personal pension on your behalf. If you would like to discuss education planning please contact me.'
 
-// // add a click event listener to the element to trigger the color change
-// myElement.addEventListener('click', () => {
-//   myElement.style.background = 'blue';
-// });
+const investmentsTxtContent = 'One key to investment success is developing a tax efficient strategy – one that minimizes the amount of taxes you have to pay on the money your investments earn. Reducing your taxes leaves more money to contribute to your investments. A financial advisor can help you use charitable giving and investment strategies to minimize the amount you must pay in taxes, and they can show you how to withdraw your money in retirement in a way that also minimizes your tax burden.';
 
+const retirementTxtContent = 'Making sure you have enough money to live on after you retire is an important part of financial planning. Planning and saving for retirement usually begin early in your career, but as retirement day gets closer, you’ll want to review and possibly revise the plan. If you didn’t start early, it’s not too late to plan for a comfortable retirement. A financial advisor can help you quantify and prioritize retirement goals, make a plan to accumulate and invest your money.';
 
+// create a copy of the button classes
+const education = document.querySelector('.education');
+const personal = document.querySelector('.personal');
+const investments = document.querySelector('.investments');
+const retirement = document.querySelector('.retirement');
 
-const p1 = document.querySelector('.p1');
-const p2 = document.querySelector('.p2');
-const p3 = document.querySelector('.p3');
-const p4 = document.querySelector('.p4');
-const p5 = document.querySelector('.p5');
-const p6 = document.querySelector('.p6');
-const p7 = document.querySelector('.p7');
-const p8 = document.querySelector('.p8');
-const p9 = document.querySelector('.p9');
-const p10 = document.querySelector('.p10');
-const p11 = document.querySelector('.p11');
-const p12 = document.querySelector('.p12');
+const explainTxt = document.querySelector('.explainTxt')
 
-let explain1 = document.querySelector('.explain1')
+education.addEventListener('click', seeTxt1);
 
-p1.addEventListener('click', seeTxt1);
 
 function seeTxt1() {
-    explain1.textContent = 'What would happen if you become disabled or develop a chronic illness and cannot take care of yourself? One of the largest expenses you could face later in life is long-term care. With the right plan in place, you can have peace of mind knowing that you will have the care you need if that day ever comes. A financial advisor can help you understand the different options available to you for long-term care planning and how those options can prepare you and your family to manage the future costs of ageing.';
-  
+    const divElement = document.querySelector('.explainTxt');
+
+    if (divElement.textContent.trim().length > 0) {
+        explainTxt.style.opacity = '0';
+        setTimeout(addTxt, 2000);
+        function addTxt() {
+            explainTxt.textContent = educationTxtContent;
+            explainTxt.style.opacity = '1';
+        }
+    } else {
+        explainTxt.textContent = educationTxtContent;
+        explainTxt.style.opacity = '1';
+
+    }
 }
 
-p2.addEventListener('click', seeTxt2);
+personal.addEventListener('click', seeTxt2);
 
 function seeTxt2() {
-    explain1.textContent = 'Making decisions about how you will be taken care of if you become incapacitated and what will happen to your assets when you die are essential parts of your total wealth strategy. The estate planning process involves creating financial and legal documents and can begin when you are young. A financial advisor can help you create a robust estate plan and regularly review that plan to ensure that it reflects your wishes and goals specific to how you want your wealth transferred.';
+    const divElement = document.querySelector('.explainTxt');
+
+    if (divElement.textContent.trim().length > 0) {
+        explainTxt.style.opacity = '0';
+        setTimeout(addTxt, 2000);
+        function addTxt() {
+            explainTxt.textContent = personalTxtContent;
+            explainTxt.style.opacity = '1';
+        }
+    } else {
+        explainTxt.textContent = personalTxtContent;
+        explainTxt.style.opacity = '1';
+
+    }
 }
 
-p3.addEventListener('click', seeTxt3);
+investments.addEventListener('click', seeTxt3);
 
 function seeTxt3() {
-    explain1.textContent = 'Charitable planning services help you make educated decisions about how to support the causes that are important to you. With the right plan in place, your money can go further to help the organizations whose missions are aligned with your values. A financial advisor can help you define your charitable giving goals and incorporate them into your financial plan. They can also advise you in appropriate ways to maximize your giving and tax deductions.';
+    const divElement = document.querySelector('.explainTxt');
+
+    if (divElement.textContent.trim().length > 0) {
+        explainTxt.style.opacity = '0';
+        setTimeout(addTxt, 2000);
+        function addTxt() {
+            explainTxt.textContent = investmentsTxtContent;
+            explainTxt.style.opacity = '1';
+        }
+    } else {
+        explainTxt.textContent = investmentsTxtContent;
+        explainTxt.style.opacity = '1';
+    }
 }
 
-p4.addEventListener('click', seeTxt4);
+retirement.addEventListener('click', seeTxt4);
 
 function seeTxt4() {
-    explain1.textContent = 'Some people begin to make a plan for paying college tuition as soon as their child is born.. Even if you didn’t start early, college planning can help you put your child through college without facing unexpectedly large expenditures. A financial advisor can guide you in understanding the best ways to save for future college costs and how to fund potential gaps, explain how to reduce out-of-pocket college costs, and advise you on eligibility for financial aid and grants.';
-}
+    const divElement = document.querySelector('.explainTxt');
 
-p5.addEventListener('click', seeTxt5);
-
-function seeTxt5() {
-    explain1.textContent = 'One key to investment success is developing a tax efficient strategy – one that minimizes the amount of taxes you have to pay on the money your investments earn. Reducing your taxes leaves more money to contribute to your investments. A financial advisor can help you use charitable giving and investment strategies to minimize the amount you must pay in taxes, and they can show you how to withdraw your money in retirement in a way that also minimizes your tax burden.';
-}
-
-p6.addEventListener('click', seeTxt6);
-
-function seeTxt6() {
-    explain1.textContent = 'Similar to estate planning, business succession planning involves making decisions about what will happen to your business if you are no longer able to work or when you decide to retire. If your business is a partnership, you will want to go through the succession planning process together. A financial advisor can help you and your partners understand the important components in business succession planning, determine the value of the business, create shareholder agreements, establish a compensation structure for successors, outline transition options, and much more.';
-}
-
-p7.addEventListener('click', seeTxt7);
-
-function seeTxt7() {
-    explain1.textContent = 'With a personal pension you pay regular monthly amounts or a lump sum to a pension provider who invests it on your behalf. Other people and family members can pay a personal pension on your behalf.';
-}
-
-p8.addEventListener('click', seeTxt8);
-
-function seeTxt8() {
-    explain1.textContent = 'Making sure you have enough money to live on after you retire is an important part of financial planning. Planning and saving for retirement usually begin early in your career, but as retirement day gets closer, you’ll want to review and possibly revise the plan. If you didn’t start early, it’s not too late to plan for a comfortable retirement. A financial advisor can help you quantify and prioritize retirement goals, make a plan to accumulate and invest your money,';
-}
-
-p9.addEventListener('click', seeTxt9);
-
-function seeTxt9() {
-    explain1.textContent = 'The basics of investment management include buying and selling financial assets and other investments, but it is more than that. Managing your investments involves understanding your short- and long-term goals and using that information to make thoughtful investing decisions. A financial advisor can provide the data necessary to help you diversify your investment portfolio to match your desired level of risk and meet your financial goals.';
-}
-
-p10.addEventListener('click', seeTxt10);
-
-function seeTxt10() {
-    explain1.textContent = 'Most people borrow money for large purchases like a home or a vehicle. Sometimes a medical bill or home repair can unexpectedly add to your debt load. A professional debt management plan helps you pay off that debt in the most financially advantageous way possible. A financial advisor can help you analyze your debt, prioritize a debt repayment strategy, provide options for debt restructuring, and outline a holistic plan to better manage debt and meet your future financial goals.';
-}
-
-p11.addEventListener('click', seeTxt11);
-
-function seeTxt11() {
-    explain1.textContent = 'Usually applied to business finances, cash flow analysis helps you balance money coming in with expenditures and savings plans. Personal cash flow analysis can tell you when you can afford to buy a new car or how much money you can add to your savings each month without running short for necessary expenses. A financial advisor can help you clearly see where you spend your money and then apply that insight to help you understand your financial well-being and how to improve it.';
-}
-
-p12.addEventListener('click', seeTxt12);
-
-function seeTxt12() {
-    explain1.textContent = 'Risk, when not managed properly, can be very expensive. Risk management services identify potential risks to your home, your vehicle, and your family, and they help you put the right insurance policies in place to mitigate those risks. A financial advisor can help you develop a strategy to protect your earning power and reduce losses when unexpected things happen.';
+    if (divElement.textContent.trim().length > 0) {
+        explainTxt.style.opacity = '0';
+        setTimeout(addTxt, 2000);
+        function addTxt() {
+            explainTxt.textContent = retirementTxtContent;
+            explainTxt.style.opacity = '1';
+        }
+    } else {
+        explainTxt.textContent = retirementTxtContent;
+        explainTxt.style.opacity = '1';
+    }
 }
 
 
+const download = document.querySelector('.download');
 
-
+download.addEventListener('click', () => {
+    alert('This would trigger a download on a live site.')
+});
 
